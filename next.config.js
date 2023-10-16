93473afc41e03274fe74474f96733d90f6669830
WebsiteArtificers <samuelteigland@websiteartificers.com>
2023-10-16 14:35:10 -0500

    fixed bug

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-          test: /\.css$/,
-          use: [
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  config: './postcss.config.js',
-                },
-              },
-            },
-          ],
-        });
-    
-        return config;
-    },
+    modules: true,
 }
 
 module.exports = nextConfig
